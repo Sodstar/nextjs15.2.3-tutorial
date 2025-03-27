03609f5ad09c5d2c2beaa1823de3c0851ef1d14a
SOD-OD BATZORIG <sodstar@Sodstars-MacBook-Pro.local>
2025-03-27 19:50:56 +0800

    hehe

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -3,44 +3,31 @@ import React, { useState, useEffect, use } from "react";
 import { products } from "@/data/products";
 import IProduct, { ProductParams } from "@/types/Types";
 
- function ProductDetail({ params }: { params: Promise<{ id: string }> }) {
+function ProductDetail({ params }: { params: Promise<{ id: string }> }) {
   const { id } = use(params);
-  const filteredProduct = products.filter((p) => p.id == Number(id));
-
+  let filteredProduct = products.filter((p) => p.id == Number(id));
+  const [counter2, setCounter2] = useState(0);
   const [counter, setCounter] = useState(0);
   const [cartItems, setCartItem] = useState<IProduct[]>([]);
 
   if (filteredProduct.length == 0) return <div>Product not found</div>;
 
   useEffect(() => {
-
-    const fetchDogs = async () => {
-      const dogs = await fetch(`https://dogapi.dog/api/v2/breeds`);
-      console.log(dogs)
-    };
+    console.log(filteredProduct);
 
     console.log("Counter Effected");
-
-
-    fetchDogs();
   }, [counter]);
 
-
   useEffect(() => {
-
- 
     console.log("Cart items Effected");
-
-
-  
   }, [cartItems]);
-
 
   const addToCart = (item: IProduct[]) => {
     setCartItem(item);
   };
 
   const countingCart = (type: string) => {
+    setCounter2(counter2+1);
     if (type == "+") setCounter(counter + 1);
     else {
       if (counter < 1) return;
@@ -71,7 +58,7 @@ import IProduct, { ProductParams } from "@/types/Types";
       >
         Minus
       </button>
-      <span className="text-lg font-bold">({counter})</span>
+      <span className="text-lg font-bold">({counter})({counter2})</span>
 
       <div className="flex items-center justify-between w-full">
         {cartItems.map((item) => (
